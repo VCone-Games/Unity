67617d3b4d00b4f8f39b49f52ea40fc66e5427dd
Enrique Tamajón Castilla <tamcas02@gmail.com>
2023-10-18 18:11:22 +0200

    Jump y movimiento horizontal incorporados
    Se ha incluido:
     - Funcionalidad del movimiento horizontal
     - Salto con "coyote timer" implementado
    
    Se ha eliminado:
     - Input Action duplicado
    
    Intentando bufferJumping y Double Jump

--- Assets/Scripts/Player Controller/Horizontal Movement.cs
+++ Assets/Scripts/Player Controller/Horizontal Movement.cs
@@ -1,18 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HorizontalMovement : MonoBehaviour
 {
+    [Header("Input system")]
+    [SerializeField] InputActionReference movementReference;
+
+    [Header("Movement params")]
+    [SerializeField] float movementSpeed;
+
+    float playerDirection;
+    Rigidbody2D myRigidbody;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        movementReference.action.performed += OnPressed;
+        movementReference.action.canceled += OnRelease;
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+	private void OnPressed(InputAction.CallbackContext context)
+	{
+		playerDirection = movementReference.action.ReadValue<float>();
+	}
+
+	private void OnRelease(InputAction.CallbackContext context)
+	{
+		playerDirection = 0.0f;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
     {
-        
+       myRigidbody.velocity = new Vector2 (playerDirection * movementSpeed, myRigidbody.velocity.y);
+
     }
+
 }
--- Assets/Scripts/Player Controller/Jump.cs
+++ Assets/Scripts/Player Controller/Jump.cs
@@ -1,18 +1,81 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Jump : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+	[Header("Input system")]
+	[SerializeField] InputActionReference jumpReference;
+
+	[Header("Jump params")]
+	[SerializeField] float jumpForce;
+	[SerializeField] float coyoteTime;
+	//[SerializeField] float bufferTime;
+	[SerializeField] float bonusAirTimeInterval;
+
+	[Header("Control variables")]
+	[SerializeField] bool jumping;
+	[SerializeField] bool isGrounded;
+	[SerializeField] float coyoteTimer;
+	//[SerializeField] float bufferTimer;
+
+	Rigidbody2D myRigidbody;
+
+	// Start is called before the first frame update
+	void Start()
     {
-        
+		jumpReference.action.performed += OnJump;
+		jumpReference.action.canceled += OnJumpCanceled;
+
+		myRigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+	private void OnJump(InputAction.CallbackContext context)
+	{
+		//Debug.Log("He pulsado saltar");
+		if (coyoteTimer > 0.0f)
+		{
+			jumping = true;
+			myRigidbody.velocity += new Vector2(0, jumpForce);
+			coyoteTimer = 0;
+			//bufferTimer = bufferTime;
+		}
+	}
+
+	void OnJumpCanceled(InputAction.CallbackContext context)
+	{
+		if (jumping)
+		{
+			jumping = false;
+
+			if (myRigidbody.velocity.y < 0) return;
+
+			myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 0);
+		}
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
     {
-        
-    }
+		coyoteTimer = isGrounded ? coyoteTime : coyoteTimer - Time.deltaTime;
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Floor")
+		{
+			isGrounded = true;
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Floor")
+		{
+			isGrounded = false;
+		}
+	}
 }
