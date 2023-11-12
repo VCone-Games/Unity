50b0612723751def499008a9b6f5212434de5130
Enrique Tamajón Castilla <tamcas02@gmail.com>
2023-11-12 21:44:32 +0100

    Azafran completo (faltan animaciones y detalles)

--- Assets/Scripts/Enemies/Bosses/Azafran/Phases/PhaseManagerAzafran.cs
+++ Assets/Scripts/Enemies/Bosses/Azafran/Phases/PhaseManagerAzafran.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class PhaseManagerAzafran : MonoBehaviour
@@ -9,23 +10,49 @@ public class PhaseManagerAzafran : MonoBehaviour
 
 	[Header("Phase Components")]
     [SerializeField] private FirstPhaseAzafran firstPhaseComponent;
-    [SerializeField] private SecondPhaseAzafran secondPhaseComponent;
-
+	[SerializeField] private Transform secondPhaseSpawn;
+	[SerializeField] private float speed = 1f;
+	
     [Header("Azafran Components")]
     [SerializeField] private HealthManagerAzafran healthManager;
+
+	private Rigidbody2D myRigidbody2D;
+	[SerializeField] private bool firstPhaseEnded = false;
+	[SerializeField] private bool secondPhaseBegin = false;
 
     // Start is called before the first frame update
     void Start()
     {
 		healthManager.EventSecondPhase += ActivateSecondPhase;
-
-		firstPhaseComponent.enabled = true;
-		secondPhaseComponent.enabled = false;
+		myRigidbody2D = GetComponent<Rigidbody2D>();
 	}
 
 	private void ActivateSecondPhase(object sender, EventArgs e)
 	{
-        firstPhaseComponent.enabled = false;
-        secondPhaseComponent.enabled = true;
+		Destroy(firstPhaseComponent);
+
+		myRigidbody2D.isKinematic = false;
+		firstPhaseEnded = true;
+		Debug.Log("Cambio de fase");
 	}
+
+	private void FixedUpdate()
+	{
+		if (firstPhaseEnded && !secondPhaseBegin)
+		{
+			Debug.Log("Transicionando...");
+			Vector3 direction = (secondPhaseSpawn.position - transform.position);
+			myRigidbody2D.velocity = direction.normalized * speed;
+		}
+
+		if (firstPhaseEnded && !secondPhaseBegin && Vector3.Distance(transform.position, secondPhaseSpawn.position) < 0.5f)
+		{
+			gameObject.AddComponent<SecondPhaseAzafran>();
+			secondPhaseBegin = true;
+		}
+
+	}
+
+	//6 cotas de malla, 6 cuero tachonado, 6 ballestas de mano, 20 cajas de ballesta (20), 6 espadas cortas, 10 dagas.
+	//6 bolsa de abrojos, 4 rollos de cuerda, 
 }
