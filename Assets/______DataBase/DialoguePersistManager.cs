a0a9ec405a8783a5ff06283ba2ddbd941c8fb4e4
Enrique Tamajón Castilla <tamcas02@gmail.com>
2023-12-12 20:34:10 +0100

    Jengibre arreglado, falta probarlo

--- Assets/______DataBase/DialoguePersistManager.cs
+++ Assets/______DataBase/DialoguePersistManager.cs
@@ -12,9 +12,20 @@ public class DialoguePersistManager : MonoBehaviour
 	[SerializeField] private List<bool> pandaCanDialogueList = new List<bool>() { true, false, false };
 
 
-    public Dictionary<string, int> NPCCurrentText { get { return npcCurrentText; } }
+	[SerializeField] private List<bool> jengibreFirstAssingsList = new List<bool>() { true, true, true, true };
+	[SerializeField] private List<bool> jengibreCanDialogueList = new List<bool>() { true, false, false, false };
+	private bool azafranReached;
+	private bool curcumaReached;
+
+	public Dictionary<string, int> NPCCurrentText { get { return npcCurrentText; } }
     public List<bool> PandaFirstAssingsList { get { return pandaFirstAssingsList; } }
     public List<bool> PandaCanDialogueList { get { return pandaCanDialogueList; } }
+
+	public List<bool> JengibreFirstAssingsList { get { return jengibreFirstAssingsList; } }
+	public List<bool> JengibreCanDialogueList { get { return jengibreCanDialogueList; } }
+
+	public bool _AzafranEndConversationReached { get { return azafranReached; } set { azafranReached = value; } }
+	public bool _CurcmuaEndConversationReached { get { return curcumaReached; } set { curcumaReached = value; } }
 
 	private void Awake()
 	{
